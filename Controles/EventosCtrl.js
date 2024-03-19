0d368d45ced1678a49349bb314754463561eeb15
carrielzada <offcarriel@gmail.com>
2024-03-19 20:30:56 -0300

    Finalizando

--- Controles/EventosCtrl.js
+++ Controles/EventosCtrl.js
@@ -60,39 +60,40 @@ gravar(requisicao, resposta){
 }
 
 
-atualizar(requisicao, resposta); {
+function atualizar(requisicao, resposta) {
     resposta.type('application/json');
-    if (requisicao.method === "PATCH" || requisicao.method === "PUT" && requisicao.is('application/json')) {
-        const dados = requisicao.body; //extrair dados do corpo da requisição
-        //Código será extraído da URL, exemplo http://localhost:3000/evento/1 // 1 é o código
-        const codigo = requisicao.params.codigo;
+    if ((requisicao.method === "PATCH" || requisicao.method === "PUT") && requisicao.is('application/json')) {
+        const dados = requisicao.body; // extrair dados do corpo da requisição
+        // Código será extraído da URL, exemplo http://localhost:3000/evento/1 // 1 é o código
+        const codigo = parseInt(requisicao.params.codigo);
         const artista = dados.artista;
         const endereco = dados.endereco;
         const cidade = dados.cidade;
         const estado = dados.estado;
         const preco = dados.preco;
-        const ingressos = dados.ingressos
+        const ingressos = dados.ingressos;
         if (codigo && codigo > 0 && artista && endereco && cidade && estado && preco && ingressos) {
             const evento = new Evento(codigo, artista, endereco, cidade, estado, preco, ingressos);
-            evento.atualizar().then(() => {
-                resposta.status(200);
-                resposta.json({
-                    "status": true,
-                    "mensagem": "Evento atualizado com sucesso!"
+            evento.atualizar()
+                .then(() => {
+                    resposta.status(200);
+                    resposta.json({
+                        "status": true,
+                        "mensagem": "Evento atualizado com sucesso!"
+                    });
+                }).catch((erro) => {
+                    resposta.status(500);
+                    resposta.json({
+                        "status": false,
+                        "mensagem": "Não foi possível atualizar o evento! " + erro.message
+                    });
                 });
-            }).catch((erro) => {
-                resposta.status(500);
-                resposta.json({
-                    "status": false,
-                    "mensagem": "Não foi possível atualizar o evento! " + erro.message
-                })
-            })
         } else {
             resposta.status(400);
             resposta.json({
                 "status": false,
                 "mensagem": "Por favor, informe todos os dados do evento conforme documentação da API"
-            })
+            });
         }
     } else {
         resposta.status(405);
@@ -103,45 +104,45 @@ atualizar(requisicao, resposta); {
     }
 }
 
-excluir(requisicao, resposta);{
+
+function excluir(requisicao, resposta) {
     resposta.type('application/json');
-    if (requisicao.method == "DELETE"){
-        //O Código do evento que será excluído será extraído da URL
-        const codigo = requisicao.params.codigo;
-        if (codigo && codigo > 0){
+    if (requisicao.method === "DELETE") {
+        // O Código do evento que será excluído será extraído da URL
+        const codigo = parseInt(requisicao.params.codigo);
+        if (codigo && codigo > 0) {
             const evento = new Evento(codigo);
             evento.excluir()
-            .then(()=>{
-                resposta.status(200);
-                resposta.json({
-                    "status":true,
-                    "mensagem": "Evento excluído com sucesso!",
+                .then(() => {
+                    resposta.status(200);
+                    resposta.json({
+                        "status": true,
+                        "mensagem": "Evento excluído com sucesso!"
+                    });
                 })
-            })
-            .catch((erro) => {
-                resposta.status(500);
-                resposta.json({
-                    "status":false,
-                    "mensagem": "Não foi possível excluir evento! " + erro.message
-                })
-            })
-        }
-        else{
+                .catch((erro) => {
+                    resposta.status(500);
+                    resposta.json({
+                        "status": false,
+                        "mensagem": "Não foi possível excluir o evento! " + erro.message
+                    });
+                });
+        } else {
             resposta.status(400);
             resposta.json({
                 "status": false,
                 "mensagem": "Por favor, informe o código do evento que deseja excluir, conforme documentação da API."
-            })
+            });
         }
-    }
-    else{
+    } else {
         resposta.status(405);
         resposta.json({
             "status": false,
             "mensagem": "Requisição inválida! Esperando o método DELETE para excluir o evento."
-        })
+        });
     }
-};
+}
+
 
 consultar(requisicao, resposta);{
     resposta.type('application/json');
