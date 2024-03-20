f04cac5bfc4fff252974ec3a6419532c34ab1dc5
carrielzada <offcarriel@gmail.com>
2024-03-19 21:09:40 -0300

    Finalizado!

--- Rotas/rotaEvento.js
+++ Rotas/rotaEvento.js
@@ -3,18 +3,19 @@
 //Por exemplo: https://localhost:3000/evento <--- evento é um endpoint
 //             domínio da aplicação   endpoint
 
-import {Router} from 'express';
+import { Router } from 'express';
 import EventoCtrl from '../Controles/EventosCtrl.js';
 
 const rotaEvento = new Router();
-const EventCtrl = new EventoCtrl();
+const eventCtrl = new EventoCtrl();
 
 rotaEvento
-.get('/:termo', eventCtrl.consultar) //Atribuindo a função consultar como parâmetro do que executar quando receber um método get na rota
+.get('/', eventCtrl.consultar)
+.get('/:termo', eventCtrl.consultar)  //atribuindo a função consultar como parâmetro do que executar quando receber um método get na rota
 .post('/', eventCtrl.gravar)
 .put('/:codigo', eventCtrl.atualizar)
 .patch('/:codigo', eventCtrl.atualizar)
-.delete('/:codigo', EventoCtrl.excluir);
+.delete('/:codigo', eventCtrl.excluir);
 
 
 export default rotaEvento;
